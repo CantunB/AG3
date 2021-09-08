7785649c8060f622475e1200a1bda3bd7f7d03e7
bernacantun <cantunberna@gmail.com>
2021-09-08 02:03:13 -0500

    Implementacion del tema Ubold modern... faltan correcciones de algunas vistas, login y home funcionando.

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-
 Route::resource('agencias', 'AgencieController');
-
 
 Route::resources([
     'usuarios' => 'UserController',
