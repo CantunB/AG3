022753eb38ed58c237523c20de534f76d5a0989d
bernacantun <cantunberna@gmail.com>
2021-09-12 03:20:35 -0500

    Migracion para los registros

--- database/migrations/2021_09_12_001204_create_registers_table.php
+++ database/migrations/2021_09_12_001204_create_registers_table.php
@@ -15,6 +15,18 @@ class CreateRegistersTable extends Migration
     {
         Schema::create('registers', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->foreignId('agency_id')->references('id')->on('agencies');
+            $table->foreignId('type_service_id')->references('id')->on('type_services');
+            $table->foreignId('airline_id')->references('id')->on('airlines');
+            $table->string('terminal')->nullable();
+            $table->dateTime('flight_time', 0);
+            $table->string('destiny')->nullable();
+            $table->string('passenger')->nullable();
+            $table->string('passenger_number')->nullable();
+            $table->dateTime('pickup', 0);
+            $table->string('requested_unit');
+            $table->string('place_service');
             $table->timestamps();
         });
     }
