bc66129d5fec97e47bc092d8295f6116afe31473
bernacantun <cantunberna@gmail.com>
2023-08-01 21:23:48 -0600

    perf: Mejoras en rendimiento de codigo

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -9,45 +9,43 @@ use Yajra\DataTables\DataTables;
 
 class BookingController extends Controller
 {
-    public function index(Request $request)
+    public function __construct()
     {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request){
         if ($request->ajax()){
             $bookings = Booking::all();
             return DataTables::of($bookings)
             ->addIndexColumn()
-            // ->editColumn('contact', function($agencies){
-            //      return $agencies->contact ?? 'Sin contacto';
-            // })
-            // ->addColumn('services', function($agencies){
-            //     return $contador = '<span class="badge badge-blue">'.$agencies->services->count().'</span>';
-            // })
-            ->addColumn('booking_id', function($bookings){
-                return $bookings->id;
+            ->addColumn('slug', function($bookings){
+                return $bookings->slug;
                 // ->created_at->toFormattedDateString();
             })
-            ->editColumn('created_at', function($bookings){
-                // $fecha = Carbon::parse($bookings->created_at);
-                return ' '.$bookings->created_at->Format('jS F Y').' <small class="text-muted">'.$bookings->created_at->isoFormat('h:mm a').'</small>';
-                //$bookings->created_at->toFormattedDateString();
-            })
-            ->editColumn('origin', function($bookings){
-                return $bookings->origin;
-            })
-            ->editColumn('destiny', function($bookings){
-                return $bookings->destiny;
-            })
+            // ->editColumn('created_at', function($bookings){
+            //     // $fecha = Carbon::parse($bookings->created_at);
+            //     return ' '.$bookings->created_at->Format('jS F Y').' <small class="text-muted">'.$bookings->created_at->isoFormat('h:mm a').'</small>';
+            //     //$bookings->created_at->toFormattedDateString();
+            // })
+            // ->editColumn('origin', function($bookings){
+            //     return $bookings->origin;
+            // })
+            // ->editColumn('destiny', function($bookings){
+            //     return $bookings->destiny;
+            // })
             ->editColumn('total', function($bookings){
-                return $bookings->price;
+                return  '<span class="badge badge-secondary">$ '.$bookings->price_mx.' MXN</span>' . '</br>' . '<span class="badge badge-outline-secondary">$ '.$bookings->price .' '. $bookings->divisa .'</span>';
             })
             ->editColumn('payment_method', function($bookings){
                 return $bookings->type_payment;
             })
             ->editColumn('payment_status', function($bookings){
                 $opciones = '';
-                if ($bookings->status_payment === null ) {
-                    $opciones .= '<h5><span class="badge bg-soft-warning text-warning"><i class="mdi mdi-timer-sand"></i> ESPERANDO PAGO</span></h5>';
+                if ($bookings->status_payment == null ) {
+                    $opciones .= '<h5><span class="badge badge-outline-warning"><i class="mdi mdi-timer-sand"></i> ESPERANDO PAGO</span></h5>';
                 }
-                elseif($bookings->status_payment === 1) {
+                elseif($bookings->status_payment == 1) {
                     $opciones .= '<h5><span class="badge bg-soft-success text-success"><i class="mdi mdi-coin"></i> PAGADO</span></h5>';
                 }
                 // '<h5><span class="badge bg-soft-success text-success"><i class="mdi mdi-coin"></i> Paid</span></h5>
@@ -58,39 +56,45 @@ class BookingController extends Controller
             })
             ->editColumn('booking_status', function($bookings){
                 $opciones = '';
-                if ($bookings->status_booking === null ) {
-                    $opciones .= '<h5><span class="badge badge-warning">PROCESANDO</span></h5>';
+                if ($bookings->status_booking == null ) {
+                    $opciones .= '<h5><span class="badge badge-outline-warning">PROCESANDO</span></h5>';
                 }
-                elseif($bookings->status_booking === 1) {
+                elseif($bookings->status_booking == 1) {
                     $opciones .= '<h5><span class="badge badge-info">Shipped</span></h5>';
                 }
-                elseif($bookings->status_booking === 2) {
+                elseif($bookings->status_booking == 2) {
                     $opciones .= '<h5><span class="badge badge-success">En servicio</span></h5>';
                 }
-                elseif($bookings->status_booking === 4) {
+                elseif($bookings->status_booking == 4) {
                     $opciones .= ' <h5><span class="badge badge-danger">Cancelado</span></h5>';
                 }
                 return $opciones;
-
-        // <h5><span class="badge badge-info">Shipped</span></h5>
-        // <h5><span class="badge badge-success">Delivered</span></h5>
-        // <h5><span class="badge badge-warning">Processing</span></h5>
-        // <h5><span class="badge badge-danger">Cancelled</span></h5>
-
             })
-            // ->addColumn('options', function ($agencies){
-            //     $opciones = '';
-            //     if (Auth::user()->can('read_agencies')) {
-            //         $opciones .= '<button type="button" onclick="btnInfo('.$agencies->id.')" class="btn action-icon icon-dual-primary"> <i class="mdi mdi-chevron-right"></i></button>';
-            //     }
-            //     if (Auth::user()->can('delete_agencies')) {
-            //         // $opciones .= '<button type="button" onclick="btnDelete('.$agencies->id.')" class="btn action-icon icon-dual-danger"> <i class="mdi mdi-delete"></i></button>';
-            //     }
-            //     return $opciones;
-            // })
-             ->rawColumns(['created_at', 'payment_status','booking_status'])
+            ->addColumn('options', function ($bookings){
+                $opciones = '';
+                // if (Auth::user()->can('read_agencies')) {
+                    $opciones .= '<button type="button" onclick="btnInfo('.$bookings->id.')" class="btn action-icon icon-dual-primary"> <i class="mdi mdi-eye"></i></button>';
+                // }
+                // if (Auth::user()->can('delete_agencies')) {
+                    $opciones .= '<button type="button" onclick="btnDelete('.$bookings->id.')" class="btn action-icon icon-dual-danger"> <i class="mdi mdi-delete"></i></button>';
+                // }
+                return $opciones;
+            })
+            ->rawColumns(['created_at', 'payment_status','total','booking_status','options'])
             ->toJson();
         }
         return view('bookings.index');
+    }
+
+    public function show(Booking $booking){
+    }
+
+    public function edit(Booking $booking){
+    }
+
+    public function update(Request $request, Booking $booking){
+    }
+
+    public function destroy(Booking $booking){
     }
 }
--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -1,73 +1,140 @@
 @extends('layouts.app')
 @section('content')
-
+<style>
+    .select2-container--default .select2-results__option--highlighted[aria-selected] {
+        background-color:#FFD038;
+    }
+    table.dataTable tbody tr.selected {
+        color: rgb(252, 252, 252);
+        background-color: #F0CA63;  /* Not working */
+    }
+    .text-wrap{
+        white-space:normal;
+    }
+    width-100{
+        width:100px;
+    }
+    .width-200{
+        width:200px;
+    }
+</style>
     <!-- Start Content-->
     <div class="container-fluid">
         @component('layouts.includes.components.breadcrumb')
-            @slot('title') {{ config('app.name', 'Laravel') }} @endslot
-            @slot('subtitle') {{ __('translation.Bookings') }} @endslot
-            @slot('teme') {{ __('translation.List') }} @endslot
+            @slot('title')
+                {{ config('app.name', 'Laravel') }}
+            @endslot
+            @slot('subtitle')
+                {{ __('translation.Bookings') }}
+            @endslot
+            @slot('teme')
+                {{ __('translation.List') }}
+            @endslot
         @endcomponent
 
-        <!-- end page title -->
-
-                            <div>
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card-box">
+                    {{--  <div>
                                     @livewire('bookings.bookings')
-                            </div>
-                            {{-- <table id="table_bookings" class="table table-centered table-nowrap mb-0">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th style="width: 20px;">
-                                            <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                                <label class="custom-control-label" for="customCheck1">&nbsp;</label>
-                                            </div>
-                                        </th>
-                                        <th>Reserva ID</th>
-                                        <th>Fecha</th>
-                                        <th>Origen</th>
-                                        <th>Destino</th>
-                                        <th>Total</th>
-                                        <th>Metodo pago</th>
-                                        <th>Estatus pago</th>
-                                        <th>Estatus reserva</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+                            </div>  --}}
+                    {{--  <table id="table_bookings" class="table table-centered table-nowrap mb-0">  --}}
+                    <table id="table_bookings"
+                        class="table table-sm table-centered border dt-responsive nowrap w-100 no-footer dtr-inline">
 
+                        <thead class="thead-light">
+                            <tr>
+                                <th style="width: 20px;">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                        <label class="custom-control-label" for="customCheck1">&nbsp;</label>
+                                    </div>
+                                </th>
+                                <th>Folio Reserva</th>
+                                <th>Servicio</th>
+                                <th>Metodo pago</th>
+                                <th>Estatus pago</th>
+                                <th>Estatus reserva</th>
+                                <th>Total</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                                </tbody>
-
-                            </table> --}}
-
-{{-- @push('scripts')
-    <script>
-        $(document).ready(function(){
-            $('#table_bookings').DataTable({
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                $('#table_bookings').DataTable({
                     processing: true,
                     serverSide: true,
                     language: {
                         "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
                     },
                     ajax: '{!! route('bookings.index') !!}',
-                    columns: [
-                        {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
-                        {data: 'booking_id', name: 'booking_id', orderable: true, searchable: true},
-                        {data: 'created_at', name: 'created_at', orderable: true, searchable: true},
-                        {data: 'origin', name: 'origin', orderable: false, searchable: false},
-                        {data: 'destiny', name: 'destiny', orderable: false, searchable: false},
-                        {data: 'total', name: 'total', orderable: false, searchable: false},
-                        {data: 'payment_method', name: 'payment_method', orderable: false, searchable: false},
-                        {data: 'payment_status', name: 'payment_status', orderable: false, searchable: false},
-                        {data: 'booking_status', name: 'booking_status', orderable: false, searchable: false},
-
-                ],
+                    columns: [{
+                            data: 'DT_RowIndex',
+                            name: 'DT_RowIndex',
+                            orderable: false,
+                            searchable: false,
+                            className: 'text-wrap width-100'
+                        },
+                        {
+                            data: 'slug',
+                            name: 'slug',
+                            orderable: true,
+                            searchable: true,
+                            className: 'text-wrap width-100'
+                        },
+                        {
+                            data: 'type_service',
+                            name: 'type_service',
+                            orderable: true,
+                            searchable: true,
+                            className: 'text-wrap width-100'
+                        },
+                        {
+                            data: 'payment_method',
+                            name: 'payment_method',
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'payment_status',
+                            name: 'payment_status',
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'booking_status',
+                            name: 'booking_status',
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'total',
+                            name: 'total',
+                            orderable: false,
+                            searchable: false,
+                            className: 'text-center'
+                        },
+                        {
+                            data: 'options',
+                            name: 'options',
+                            orderable: false,
+                            searchable: false
+                        }
+                    ],
+                });
             });
-        });
-    </script>
-@endpush --}}
-    @push('modals')
-        <livewire:bookings.live-modal>
+        </script>
     @endpush
+    {{--  @push('modals')
+        <livewire:bookings.live-modal>
+    @endpush  --}}
 @endsection
-
